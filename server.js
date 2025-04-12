b23b404723a99171e23002eb18c16be42999ed88
roomkartz <contact.roomkartz@gmail.com>
2025-04-13 00:42:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,12 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // Optional: Enable CORS if your frontend runs on a different domain/port
-// const corsOptions = {
-//   origin: "*",
-//   methods: "GET,POST,PUT,DELETE,PATCH",
-//   credentials: true,
-// };
-// app.use(cors(corsOptions));
-app.use(cors());
-
+const corsOptions = {
+  origin: "https://www.roomkartz.com",
+  methods: "GET,POST,PUT,DELETE,PATCH",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use("/uploads", express.static("uploads")); // Serve static files
 
 // Routes
